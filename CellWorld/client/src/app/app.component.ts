dae72205a58b5e0a0a2a81b67bba83a9cb419b87
Kapibarabanka <kapibarabanka@gmail.com>
2021-04-01 22:44:43 +0300

    template of simulation

--- CellWorld/client/src/app/app.component.ts
+++ CellWorld/client/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from "@angular/core";
+import { timer } from "rxjs";
+import {take} from 'rxjs/operators';  
 
 @Component({
   selector: "app",
@@ -9,12 +11,12 @@ import { Component } from "@angular/core";
 export class AppComponent {
   state: number = 1;
   isMouseDown: boolean = false;
-  size: number = 5;
+  size: number = 100;
   matrix = []
 
   colors = [
-    'green', // alive
-    "purple" // dead
+    'white', // dead
+    "black" // alive
   ]
 
   constructor() {
@@ -26,7 +28,6 @@ export class AppComponent {
     }
 
     document.onmouseup = () => {
-      console.log("global mouse up");
       this.isMouseDown = false;
     };
   }
@@ -35,21 +36,22 @@ export class AppComponent {
     this.state = this.state == 0 ? 1 : 0;
   }
 
-  removeColumn() {}
-
-  shuffle() {}
+  simulate() {
+    timer(0, 1).pipe(
+      take(this.size)).subscribe(x=>{
+        this.matrix[x][x] = 1;
+       })
+  }
 
   onMouseDown(i: number, j: number) {
     this.isMouseDown = true;
     this.matrix[i][j] = this.state;
-    console.log(`mouse down; i = ${i} j = ${j}`);
     return false;
   }
 
   onMouseOver(i: number, j: number) {
     if (this.isMouseDown) {
       this.matrix[i][j] = this.state;
-      console.log(`mouse over; i = ${i} j = ${j} state = ${this.matrix[i][j].state}`);
     }
   }
 
@@ -59,5 +61,13 @@ export class AppComponent {
     }
 
     return this.colors[0];
+  }
+
+  clear(){
+    for (var i = 0; i < this.size; i++) {
+      for (var j = 0; j < this.size; j++) {
+        this.matrix[i][j] = 0;
+      }
+    }
   }
 }
