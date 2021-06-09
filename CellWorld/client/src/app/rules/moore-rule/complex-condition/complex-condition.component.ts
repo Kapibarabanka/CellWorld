71a5821c1cd18c3ad017e9eaa188ad6c3c4b2224
Kapibarabanka <kapibarabanka@gmail.com>
2021-06-09 17:39:20 +0300

    add creation of inner rules in complex rule

--- CellWorld/client/src/app/rules/moore-rule/complex-condition/complex-condition.component.ts
+++ CellWorld/client/src/app/rules/moore-rule/complex-condition/complex-condition.component.ts
@@ -1,26 +1,23 @@
-import { ComplexConditionModel } from './../condition-models/complex-condition-model';
-import { Component, Input, OnInit } from '@angular/core';
-import { ColorMap } from 'src/app/colors/color-map';
-import { ConditionModel } from '../condition-model';
+import { ComplexConditionModel } from "./../condition-models/complex-condition-model";
+import { Component, Input, OnInit } from "@angular/core";
+import { ColorMap } from "src/app/colors/color-map";
+import { ConditionModel } from "../condition-model";
+import { DirectConditionModel } from "../condition-models/direct-condition-model";
+import { SumConditionModel } from "../condition-models/sum-condition-model";
 
 @Component({
-  selector: 'complex-condition',
-  templateUrl: './complex-condition.component.html',
-  styleUrls: ['./complex-condition.component.css']
+  selector: "complex-condition",
+  templateUrl: "./complex-condition.component.html",
+  styleUrls: ["./complex-condition.component.css"],
 })
 export class ComplexConditionComponent implements OnInit {
   @Input() conditionModel: ComplexConditionModel;
   @Input() ColorMap: ColorMap;
 
-  public Operators = [
-    'AND',
-    'OR',
-    'XOR',
-    'NOT'
-  ]
+  public Operators = ["AND", "OR", "XOR", "NOT"];
 
   public get left(): ConditionModel {
-    return this.conditionModel.LeftCondition
+    return this.conditionModel.LeftCondition;
   }
 
   public get selectedOperator(): string {
@@ -28,25 +25,33 @@ export class ComplexConditionComponent implements OnInit {
   }
 
   public get right(): ConditionModel {
-    return this.conditionModel.RightCondition
+    return this.conditionModel.RightCondition;
   }
 
-  constructor() {
-  }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
-  public selectOperator(op: string){
+  public selectOperator(op: string) {
     this.conditionModel.Operator = op;
   }
 
   public addDirectRule(isLeft: boolean) {
-
+    const defaultState = 0;
+    const newCondition = new ConditionModel("direct", new DirectConditionModel(defaultState))
+    if (isLeft) {
+      this.conditionModel.LeftCondition = newCondition;
+    } else {
+      this.conditionModel.RightCondition = newCondition;
+    }
   }
 
   public addSumRule(isLeft: boolean) {
-    
+    const newCondition = new ConditionModel("sum", new SumConditionModel())
+    if (isLeft) {
+      this.conditionModel.LeftCondition = newCondition;
+    } else {
+      this.conditionModel.RightCondition = newCondition;
+    }
   }
-
 }
